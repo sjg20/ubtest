1b68f5fe18babda4b251f9375bd8e6a153ea22e8
Simon Glass <sjg@chromium.org>
2020-01-02 16:39:26 -0700

    Add jerry
    Jerry is a RK3288-based Chromebook. Add it to the lab.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ jerry.py
@@ -0,0 +1,27 @@
+import tbot
+from tbot.machine import board, channel, connector, linux
+from chromebook import Chromebook, ChromebookLinux
+
+class Jerry(
+    Chromebook
+):
+    name = "Jerry"
+    servo_name = "jerry"
+
+# Linux machine
+#
+# We use a config which boots directly to Linux without interaction
+# with a bootloader for this example.
+class JerryLinux(
+    ChromebookLinux
+):
+    # Username for logging in once linux has booted
+    username = "root"
+    # Password.  If you don't need a password, set this to `None`
+    password = "test0000"
+
+
+# tbot will check for `BOARD`, don't forget to set it!
+BOARD = Jerry
+# You need to set `LINUX` now as well.
+LINUX = JerryLinux
