7781b84d8a88b995d44c184852d21635b0e076f0
Simon Glass <sjg@chromium.org>
2020-02-01 06:04:17 -0700

    Add support for Digital Loggers Inc power

---
+++ dli.py
@@ -0,0 +1,27 @@
+import time
+import dlipower
+
+class Dli(object):
+    """Intended to be included as a subclass of the board that needs it
+
+    Properties provided by base class:
+        host: Lab host
+        dli_hostname: IP address or host name of power (e.g "192.168.0.100")
+        dli_user: User name to use (e.g. "admim")
+        dli_password: Password to use (e.g. "1234")
+        dli_outlet: Outlet to control (1-8)
+    """
+    switch = None
+
+    def dli_setup(self):
+        if not self.switch:
+            self.switch = dlipower.PowerSwitch(hostname=self.dli_hostname,
+                userid=self.dli_user, password=self.dli_password)
+
+    def dli_on(self):
+        self.dli_setup()
+        self.switch.on(self.dli_outlet)
+
+    def dli_off(self):
+        self.dli_setup()
+        self.switch.off(self.dli_outlet)
