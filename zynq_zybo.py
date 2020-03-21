cfba8759ceb41d542dd0d7d7563653a1fc2b85ff
Simon Glass <sjg@chromium.org>
2020-03-21 16:21:30 -0600

    Fix zynq_zybo

--- zynq_zybo.py
+++ zynq_zybo.py
@@ -1,7 +1,11 @@
+import os
+import socket
+import time
 import tbot
 from tbot.machine import board, channel, connector, linux
 from dli import Dli
 from sdwire import Sdwire
+from usbrelay import Usbrelay
 
 class ZynqZybo(
     connector.ConsoleConnector,
@@ -9,6 +13,7 @@ class ZynqZybo(
     board.Board,
     Dli,
     Sdwire,
+    Usbrelay
 ):
     name = "zynq_zybo"
     dli_hostname = "192.168.4.19"
@@ -16,9 +21,17 @@ class ZynqZybo(
     dli_password = "1234"
     dli_outlet = 5
 
+    usbrelay_name = "6QMBS_"
+    usbrelay_reset=6
+
     sdwire_serial = "202001064001"
 
+    uart_port = "/dev/ttyusb_port5"
     #ether_mac = "02:4f:04:03:26:d1"
+
+    def _poweron(self) -> None:
+        self.sdwire_dut()
+        self.dli_on()
 
     def poweron(self) -> None:
         """Procedure to turn power on."""
@@ -26,8 +39,7 @@ class ZynqZybo(
         # You can access the labhost as `self.host` (if you use the
         # ConsoleConnector).  In this case I have a simple command to
         # toggle power.
-        self.sdwire_dut()
-        self.dli_on()
+        self._poweron()
 
     def poweroff(self) -> None:
         """Procedure to turn power off."""
@@ -37,11 +49,15 @@ class ZynqZybo(
     def connect(self, mach) -> channel.Channel:
         """Connect to the boards serial interface."""
 
+        # Zynq's UART does not appear until the board is powered on
+        self._poweron()
+        time.sleep(.7)
+
         # `mach.open_channel` 'steals' mach's channel and runs the
         # given command to connect to the serial console.  Your command
         # should just connect its tty to the serial console like rlogin,
         # telnet, picocom or kermit do.  The minicom behavior will not work.
-        return mach.open_channel("picocom", "-b", "115200", "/dev/ttyusb_port5")
+        return mach.open_channel("picocom", "-b", "115200", self.uart_port)
 
 # Linux machine
 #
