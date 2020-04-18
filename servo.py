8bf8bdd5bc862e5877fa4b00ffc32871aff8551f
Simon Glass <sjg@chromium.org>
2020-04-18 13:16:18 -0600

    servo

--- servo.py
+++ servo.py
@@ -16,13 +16,13 @@ class Servo:
         ykush_serial: Serial number of YKUSH device
         ykush_port: Port number on that device
     """
-    servo_port = None
+    #servo_port = None
 
-    def servo_setup(self):
-        if not self.servo_port:
-            servos = cros_setup.Servos()
-            servos.read()
-            self.servo_port = servos.get_port_by_name(self.servo_name)
+    #def servo_setup(self):
+        #if not self.servo_port:
+            #servos = cros_setup.Servos()
+            #servos.read()
+            #self.servo_port = servos.get_port_by_name(self.name)
 
     def servo_on(self):
         self.servo_reset()
@@ -34,11 +34,11 @@ class Servo:
         self.dut_control("cold_reset:on", "sleep:.2", "cold_reset:off")
 
     def dut_control(self, *args):
-        self.servo_setup()
-        return self.host.exec0(DUT_CONTROL, "-p", self.servo_port, *args)
+        #self.servo_setup()
+        return self.host.exec0(DUT_CONTROL, "-p", str(self.servo_port), *args)
 
     def servo_get_tty(self):
         pty = self.dut_control("cpu_uart_pty").strip().split(':')[1]
         tbot.log.message("%s is on port %s and uses %s" %
-                         (self.servo_name, self.servo_port, pty))
+                         (self.name, self.servo_port, pty))
         return pty
